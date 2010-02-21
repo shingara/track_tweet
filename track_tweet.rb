7a05735a3595f7e13aedb754500b9a9be48f1743
Cyril Mougel <cyril.mougel@af83.com>
2010-02-21 19:48:39 +0100

    initial commit

---
+++ track_tweet.rb
@@ -0,0 +1,18 @@
+require 'tweetstream'
+require 'isaac'
+config = YAML.load_file('config.yml')
+
+configure do |c|
+  c.nick    = config['irc']['nick']
+  c.server  = config['irc']['server']
+  c.port    = config['irc']['port']
+end
+
+on :connect do
+  join config['irc']['channel']
+  TweetStream::Client.new(config['twitter']['login'],config['twitter']['password']).track(*config['twitter']['track']) do |status|
+    # The status object is a special Hash with
+    # method access to its keys.
+    msg config['irc']['channel'], status.text
+  end
+end
